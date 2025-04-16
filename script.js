16af07376f7dbfdde35e8992c79189c4a40963c1
ElineBRD <eline.brsd@gmail.com>
2025-04-16 20:11:07 +0200

    [Feature] : starting js code

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const fs = require('fs'); // ? fs = file system -> import d'un outil qui permet de lire des fichiers
+const fs = require('fs'); // fs = file system -> import d'un outil qui permet de lire des fichiers
 
-let liste_articles =[]; // ? let = permet de modifier la variable plus tard (ex: ajouter des articles)
+let liste_articles =[]; // let = permet de modifier la variable plus tard (ex: ajouter des articles)
 
 try {
-    const data = fs.readFileSync('articles.json', 'utf8');
-    liste_articles = JSON.parse(data);
-}
-catch (error) {
-    console.log("Je n'arrive pas à accéder à articles.json.")
-    liste_articles;
+    const data = fs.readFileSync('articles.json', 'utf8'); // Tout e contenu du fichier est contenu dans la variable "data"
+    liste_articles = JSON.parse(data); 
+} catch (error) {
+    console.log("Je n'arrive pas à accéder à articles.json.");
+    liste_articles = [];
 }
 
 console.log("Articles chargés :", liste_articles);
